318c0f5688af2397074e1d66acba1c1f005908de
Aleksandar Pantic <apantic@microsoft.com>
2025-03-15 22:42:22 +0100

    adjust reservation card padding on phone and web

--- src/pages/Home/BannerSection/ReservationCard.jsx
+++ src/pages/Home/BannerSection/ReservationCard.jsx
@@ -34,7 +34,7 @@ const ReservationCard = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="reservation reserve-card w-[90%] max-w-[380px] p-6 sm:p-8 mx-auto md:mx-0 md:absolute md:bottom-12 md:right-8 lg:right-16 shadow-2xl bg-zinc-800/70 backdrop-blur-md border border-zinc-700/30 rounded-[2rem] text-left text-[14px] transition-all hover:shadow-[0_20px_50px_rgba(0,0,0,0.5)] hover:border-zinc-600/40">
+    <form onSubmit={handleSubmit} className="reservation reserve-card w-[90%] max-w-[380px] p-6 sm:p-8 pt-10 mx-auto md:mx-0 md:absolute md:bottom-12 md:right-8 lg:right-16 shadow-2xl bg-zinc-800/70 backdrop-blur-md border border-zinc-700/30 rounded-[2rem] text-left text-[14px] transition-all hover:shadow-[0_20px_50px_rgba(0,0,0,0.5)] hover:border-zinc-600/40">
       <div className="flex justify-center gap-5 pb-4 relative">
         <button
           type="button"
