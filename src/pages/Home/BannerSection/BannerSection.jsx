900201b317891826cb589584e7a05cee8a6dc772
Aleksandar Pantic <apantic@microsoft.com>
2025-05-02 18:43:00 +0200

    Revert "fixing new workflow"
    This reverts commit 0da88dbc9d556a1634b6b43fcc9f418f3a46abad.

--- src/pages/Home/BannerSection/BannerSection.jsx
+++ src/pages/Home/BannerSection/BannerSection.jsx
@@ -6,10 +6,10 @@ const BannerSection = () => {
   return (
     <div className="banner relative container-big rounded-[1.5rem] text-center mt-24 pb-32 md:pb-0">
       <h1 className="text-4xl sm:text-5xl md:text-8xl font-semibold pt-8 tracking-wide">
-        Elite Way Limo
+        Nikola i lale bre Limo
       </h1>
       <p className="md:w-[42ch] mx-auto mb-8 px-4 md:px-0">
-        We offer professional car rental & limousine services in our range of
+        Novo 1Alek1 andri offer professional car rental & limousine services in our range of
         high-end vehicles
       </p>
       <Link to={"/vehicles"}>
