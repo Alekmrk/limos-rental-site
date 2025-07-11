8beccadfa0a0f81f62c0d4c1aa73331cec7f2b61
Aleksandar Pantic <apantic@microsoft.com>
2025-07-11 07:33:08 -0700

    Remove TestimonialsSection from Home page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,7 +25,6 @@ const Home = ({ scrollUp, setSelectedVehicle }) => {
       <FeaturesSection />
       <FleetSection setSelectedVehicle={setSelectedVehicle} />
       <TestimonialStatsSection />
-      <TestimonialsSection />
       <PricingTransparency />
       <EventsSection />
     </>
