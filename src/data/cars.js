1f7bb129b26a71866e24f7c66a9399495b501b66
Aleksandar Pantic <apantic@microsoft.com>
2025-08-06 15:09:03 -0700

    adding new images for firstClassVan interior

--- src/data/cars.js
+++ src/data/cars.js
@@ -10,8 +10,8 @@ import imgEClassInterior1 from "../assets/cars/eclass(i11).png";
 import imgEClassInterior2 from "../assets/cars/eclass(i22).png";
 import imgBusinessVanInterior1 from "../assets/cars/businessVan(i1).png";
 import imgBusinessVanInterior2 from "../assets/cars/businessVan(i2).png";
-import imgFirstClassVanInterior1 from "../assets/cars/businessVan(i2).png";
-import imgFirstClassVanInterior2 from "../assets/cars/businessVan(i2).png";
+import imgFirstClassVanInterior1 from "../assets/cars/firstclassVan(i111).jpg";
+import imgFirstClassVanInterior2 from "../assets/cars/firstclassVan(i222).jpg";
 
 
 let cars = [
